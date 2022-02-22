4efc7cde2f72b1645435df7cca7d754d03a03e92
Pedro Cardoso da Silva <pedro.cardoso@nitrodev.com>
2022-02-22 00:46:27 -0300

    Why can't we have nice things?

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/browser';
 import { BrowserTracing } from '@sentry/tracing';
 
-if (globalThis.ENVIRONMENT !== Environment.dev) {
+if (globalThis.ENVIRONMENT !== 'development') {
 	Sentry.init({
 		dsn: 'https://906acf3ccefd46338a368a5b78c2e713@o1149821.ingest.sentry.io/6222244',
 		release: `@${globalThis.PKG_VERSION}`,
